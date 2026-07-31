aa1de0433cc694f57c20cd46f37f8e33ac400cc5
Jens Hauser <jens.hauser@chrono24.com>
2026-07-31 12:52:26 +0200

    Add to quick setting menu

---
+++ app/src/main/java/com/github/keiki/displaytorch/TorchTileService.kt
@@ -0,0 +1,43 @@
+package com.github.keiki.displaytorch
+
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
+import android.service.quicksettings.Tile
+import android.service.quicksettings.TileService
+
+class TorchTileService : TileService() {
+
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile?.apply {
+            state = Tile.STATE_INACTIVE
+            label = getString(R.string.tile_label)
+            icon = android.graphics.drawable.Icon.createWithResource(this@TorchTileService, R.drawable.ic_tile_torch)
+            updateTile()
+        }
+    }
+
+    override fun onClick() {
+        super.onClick()
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.UPSIDE_DOWN_CAKE) {
+            val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                PendingIntent.FLAG_IMMUTABLE
+            )
+            startActivityAndCollapse(pendingIntent)
+        } else {
+            startActivityAndCollapseLegacy(intent)
+        }
+    }
+
+    @Suppress("DEPRECATION")
+    private fun startActivityAndCollapseLegacy(intent: Intent) {
+        startActivityAndCollapse(intent)
+    }
+}
